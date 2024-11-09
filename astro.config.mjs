f584cb7c2f469631bf5e12a2e3b40ee88d35b368
TheHackerBoi <107680243+tonysebastine@users.noreply.github.com>
2024-11-09 10:04:06 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
+  site: "https://astro.thbz.in/",
   integrations: [
     sitemap(),
     robotsTxt({
